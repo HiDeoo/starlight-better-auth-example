e2a4e3b78b05d98f50c0e0fef4bbcaa7d35901a5
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-10-18 09:39:15 +0200

    feat: env vars

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Database from 'better-sqlite3'
 
 export const auth = betterAuth({
   baseURL: import.meta.env.BETTER_AUTH_URL,
+  secret: import.meta.env.BETTER_AUTH_SECRET,
   database: new Database('./auth.db'),
   emailAndPassword: {
     enabled: true,
